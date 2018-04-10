c3abf58c0b5bde7e61db7a3948e2ef0d9afa7db8
mradol <tuanbk189@gmail.com>
2018-04-10 23:27:31 +1000

    Dealing with Route Params

--- src/app/3-user-details/user-details.component.spec.ts
+++ src/app/3-user-details/user-details.component.spec.ts
@@ -1,4 +1,4 @@
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
@@ -12,7 +12,21 @@ class RouterStub {
 }
 
 class ActivatedRouteStub {
-  params: Observable<any> = Observable.empty();
+  // Subject is a class defined by rxjs, it derives from Observable
+  // So it has all the capabilities that we have in observables
+  // However it has an additional feature
+  // It has a method we can call to push a new value into this observable
+  private subject = new Subject();
+
+  push(value) {
+    // next() is a method that use to push a value into observable
+    this.subject.next(value);
+  }
+
+  // Expose this subject as an observable to the outside world
+  get params() {
+    return this.subject.asObservable();
+  }
 }
 
 describe('UserDetailsComponent', () => {
@@ -37,11 +51,22 @@ describe('UserDetailsComponent', () => {
   });
 
   it('should redirect the user to the users page after saving', () => {
-    let router = TestBed.get(Router);
+    let router = TestBed.
+      get(Router);
     let spy = spyOn(router, 'navigate');
 
     component.save();
 
     expect(spy).toHaveBeenCalledWith(['users']);
+  });
+
+  it('should navigate to the not found page when an invalid user id is passed', () => {
+    let router = TestBed.get(Router);
+    let spy = spyOn(router, 'navigate');
+
+    let route: ActivatedRouteStub = TestBed.get(ActivatedRoute);
+    route.push({ id: 0 });
+
+    expect(spy).toHaveBeenCalledWith(['not-found']);
   });
 });
